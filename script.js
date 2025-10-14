d3692bc2d5b639fb29a2641d6717c423f52b28e6
YSWeb <yunielsanchez309@gmail.com>
2025-10-14 10:32:41 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function menuoc(){
+    if(menu.style.display=="none")
+    {
+setTimeout(() => {menu.style.display="block";}, 60);}
+    else{
+setTimeout(() => {menu.style.display="none";}, 60);}
+}
