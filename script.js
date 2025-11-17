c67fb2997739fdb5bc266466db72777a3e6903c4
YSWeb <yunielsanchez309@gmail.com>
2025-11-16 20:24:14 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -13,11 +13,50 @@ function menuc(){
 function busquedaoc(){
     if(busqueda.style.display=="block")
     {
-setTimeout(() => {busqueda.style.display="none";}, 60);}
+setTimeout(() => {busqueda.style.display="none";
+    q.value=""
+    tableid.style.display="none"}, 60);}
     else{
 setTimeout(() => {busqueda.style.display="block";}, 60);}
 }
 
 function clickendivapks(){
     busqueda.style.display="none"
+    q.value=""
+    tableid.style.display="none"
+}
+
+function search(){
+			if (document.getElementById("q").value=="") {document.getElementById("tableid").style.display="none"}
+			else{document.getElementById("tableid").style.display="block"}
+			var num_cols, display, input, filter, table_body, tr, td, i, txtValue;
+			num_cols = 3;
+			input = document.getElementById("q");
+			filter = input.value.toUpperCase();
+			table_body = document.getElementById("the_table_body");
+			tr = table_body.getElementsByTagName("tr");
+
+			for(i=0; i< tr.length; i++){				
+				display = "none";
+				for(j=0; j < num_cols; j++){
+					td = tr[i].getElementsByTagName("td")[j];
+					if(td){
+						txtValue = td.textContent || td.innerText;
+						if(txtValue.toUpperCase().indexOf(filter) > -1){
+							display = "";
+						}
+					}
+				}
+				tr[i].style.display = display;
+			}
+		}		
+
+function contadordedineroinfopageopen(){
+    window.location="contador de dinero.info.html"
+}
+function traductordetextoinfopageopen(){
+    window.location="Traductor de texto info.html"
+}
+function codigosdeetecsainfopageopen(){
+    window.location="Codigos de ETECSA info.html"
 }
