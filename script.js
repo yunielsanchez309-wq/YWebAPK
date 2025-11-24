2f5433604a6129b7b54010fe96085df4e2269193
YSWeb <yunielsanchez309@gmail.com>
2025-11-24 14:15:41 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,36 @@
+function menuoc(){busqueda.style.display="none"
+    if(menu.style.display=="none")
+    {
+setTimeout(() => {menu.style.display="block";}, 60);}
+    else{
+setTimeout(() => {menu.style.display="none";}, 60);}
+}
 
-        
-		function search(){
+function menuc(){
+    menu.style.display="none"
+}
+
+function busquedaoc(){
+    if(busqueda.style.display=="block")
+    {
+setTimeout(() => {busqueda.style.display="none";
+    q.value=""
+    tableid.style.display="none"}, 60);}
+    else{
+setTimeout(() => {busqueda.style.display="block";}, 60);}
+}
+
+function clickendivapks(){
+    busqueda.style.display="none"
+    q.value=""
+    tableid.style.display="none"
+}
+
+function search(){
+			if (document.getElementById("q").value=="") {document.getElementById("tableid").style.display="none"}
+			else{document.getElementById("tableid").style.display="block"}
 			var num_cols, display, input, filter, table_body, tr, td, i, txtValue;
-			num_cols = 1;
+			num_cols = 3;
 			input = document.getElementById("q");
 			filter = input.value.toUpperCase();
 			table_body = document.getElementById("the_table_body");
@@ -22,32 +50,13 @@
 				tr[i].style.display = display;
 			}
 		}		
-	
-function cerrardivvideo(){
-	contentdiv.style.display="block";
-	document.getElementById("divflotvideoview").style.display="none";
-document.getElementById("videoreproendiv").pause();
-	videoreproendiv.style.display="none";
-	img1divflotvideo.style.display="none"
-	videotitle.innerHTML="Titulo";
-	videoinfo.innerHTML="Descripción";
+
+function contadordedineroinfopageopen(){
+    window.location="contador de dinero.info.html"
 }
-function openvideodiv(){
-	contentdiv.style.display="none";
-	setTimeout(() => {videoreproendiv.play();}, 100);
-	document.getElementById("divflotvideoview").style.display="block";
+function traductordetextoinfopageopen(){
+    window.location="Traductor de texto info.html"
 }
-
-
-
-
-function openvideofiesta1(){
-	videoreproendiv.src="VID-20251118-WA0000.mp4";
-	videoreproendiv.style.display="block";
-	img1divflotvideo.style.display="block";
-videotitle.innerHTML="fiesta 1";
-	videoinfo.innerHTML="Descripcion fiesta 1";
-	setTimeout(() => {openvideodiv();}, 100);
+function codigosdeetecsainfopageopen(){
+    window.location="Codigos de ETECSA info.html"
 }
-
-
